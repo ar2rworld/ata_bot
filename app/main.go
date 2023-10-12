1ff7bc9d8c6bd5029c4496e8b1042e98f11e38e0
ar2rworld <ar2r.world@gmail.com>
2023-10-12 18:08:49 -0400

    update unban ban art functions

--- app/main.go
+++ app/main.go
@@ -31,6 +31,7 @@ func main() {
 	}
 
 	unbanArt(bot, ataStorage)
+	// banArt(bot)
 
 	bot.Debug = true
 
@@ -61,25 +62,73 @@ func main() {
 	ataBot.Start()
 }
 
-func unbanArt(bot *tgbotapi.BotAPI, ataStorage *storage.Storage){
-	artID  := int64(1265820975)
+func unbanArt(bot *tgbotapi.BotAPI, ataStorage *storage.Storage) {
+	artID := int64(1265820975)
 	chatID := int64(-1001506079405)
 	chatMemberConfig := &tgbotapi.ChatMemberConfig{
 		ChatID: chatID,
 		UserID: artID,
 	}
 
-	message := &tgbotapi.UnbanChatMemberConfig{
+	un := &tgbotapi.UnbanChatMemberConfig{
 		ChatMemberConfig: *chatMemberConfig,
 	}
 
-	_, err := bot.Send(message)
-	if err != nil && err.Error() != "json: cannot unmarshal bool into Go value of type tgbotapi.Message" {
+	res, err := bot.Request(un)
+	if err != nil {
 		log.Println("unban:", err)
+	}
+	if res.ErrorCode != 0 {
+		log.Printf("error during unban call: errorCode: %d, description: %s, json raw: %s", res.ErrorCode, res.Description, res.Result)
 	}
 	result, err := ataStorage.DB.Collection(storage.FakesCollection).DeleteOne(context.Background(), bson.D{{Key: "id", Value: artID}})
 	if err != nil {
 		log.Println(err)
 	}
 	log.Println("deleted: ", result.DeletedCount)
+}
+
+type BanRequest struct {
+	UserID         int64
+	ChatID         int64
+	revokeMessages bool
+}
+
+func (BanRequest) method() string {
+	return "banChatMember"
+}
+
+func (r BanRequest) params() (tgbotapi.Params, error) {
+	return map[string]string{
+		"user_id":         strconv.FormatInt(r.UserID, 10),
+		"chat_id":         strconv.FormatInt(r.ChatID, 10),
+		"revoke_messages": strconv.FormatBool(true),
+	}, nil
+}
+
+func banArt(bot *tgbotapi.BotAPI) {
+	// r := &BanRequest{
+	// 	UserID: int64(1265820975),
+	// 	ChatID: int64(-1001506079405),
+	// }
+	// var t tgbotapi.Chattable = r
+	// t = BanRequest{
+	// 	UserID: int64(1265820975),
+	// 	ChatID: int64(-1001506079405),
+	// }
+	artID := int64(1265820975)
+	chatID := int64(-1001506079405)
+	r := tgbotapi.BanChatMemberConfig{
+		ChatMemberConfig: tgbotapi.ChatMemberConfig{
+			ChatID: chatID,
+			UserID: artID,
+		},
+		RevokeMessages: true,
+	}
+
+	res, err := bot.Request(r)
+	if err != nil {
+		log.Println("err:", err)
+	}
+	log.Printf("err code: %d, desc: %s, json raw: %s", res.ErrorCode, res.Description, res.Result)
 }
