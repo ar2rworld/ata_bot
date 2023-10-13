ab3af463176382b10e1bb194280a7c52badb2765
ar2rworld <ar2r.world@gmail.com>
2023-10-13 13:03:15 -0400

    BadRequestMessageToDeleteNotFound added

--- app/myerror/myerrormessages.go
+++ app/myerror/myerrormessages.go
@@ -6,3 +6,5 @@ const UnmarshallBoolErrorMessage = "json: cannot unmarshal bool into Go value of
 
 const BadRequestMessageCantBeDeleted = "Bad Request: message can't be deleted"
 const BadRequestNotEnoughRights = "Bad Request: not enough rights to restrict/unrestrict chat member"
+
+const BadRequestMessageToDeleteNotFound = "Bad Request: message to delete not found"
