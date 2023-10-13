12010d6dbae5895fc4aa1f3ce72767945ad46ec5
ar2rworld <ar2r.world@gmail.com>
2023-10-13 12:38:08 -0400

    NewAPIResponseError

--- app/myerror/myerror.go
+++ app/myerror/myerror.go
@@ -1,5 +1,11 @@
 package myerror
 
+import (
+	"fmt"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
 type MyError struct {
 	message string
 }
@@ -10,4 +16,11 @@ func (e *MyError) Error () string {
 
 func NewError(m string) *MyError {
 	return &MyError{message: m,}
+}
+
+func NewAPIResponseError(res *tgbotapi.APIResponse, e error) error {
+	return NewError(
+		fmt.Sprintf(`APIResponse error: "%s", errorCode: %d, description: %s, json res: %s`,
+		e, res.ErrorCode, res.Description, res.Result),
+	)
 }
