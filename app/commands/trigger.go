5b9662ecdc33e5ceb23d8ed0866079fb4db16f89
ar2rworld <ar2r.world@gmail.com>
2023-10-20 17:11:49 -0400

    fix api report to admin

--- app/commands/trigger.go
+++ app/commands/trigger.go
@@ -184,7 +184,7 @@ func (t *Trigger) Exec(update *tgbotapi.Update) error {
 			if err != nil {
 				return err
 			}
-			report := fmt.Sprintf("user: %s with id: %d\ndata: %d", newMember.UserName, newMember.ID, resData)
+			report := fmt.Sprintf("user: %s with id: %d\ndata: %s", newMember.UserName, newMember.ID, resData)
 			err = ataBot.SendToAdmin(report)
 			if err != nil {
 				return err
